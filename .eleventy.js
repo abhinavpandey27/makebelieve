02d8fef22ab79537356655be4430e0f939ae3a94
Abhinav Pandey <adhinavpandey@Adhinavs-MacBook-Pro.local>
2021-10-18 17:48:08 +0530

    Did a lot things

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
-const htmlmin = require('html-minifier')
+const htmlmin = require('html-minifier');
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+const markdownIt = require('markdown-it')
+const markdownitlinkatt = require('markdown-it-link-attributes')
+const markdownItAnchor = require('markdown-it-anchor')
 
 
 module.exports = function(eleventyConfig) {
@@ -25,8 +29,53 @@ module.exports = function(eleventyConfig) {
       return minified
     }
 
+
     return content
   })
+
+
+  const pluginTOC = require('eleventy-plugin-nesting-toc');
+  
+  eleventyConfig.addPlugin(pluginTOC, {tags: ['h1, h2, h3, h4']}, {headingText: 'Table of Contents'});
+    	/* Markdown Overrides */
+	let markdownLibrary = markdownIt({
+		html: true,
+		breaks: true
+	})
+  /* Markdown Overrides 
+		.use(markdownitlinkatt, {
+			pattern: /^(?!(https:\/\/kailoon\.com|#)).*$/gm,
+			attrs: {
+				target: '_blank',
+				rel: 'noreferrer'
+			}
+		}) */
+		.use(markdownItAnchor, {
+      html:true,
+      linkify: true,
+      typographer: true,
+			permalink: true,
+			permalinkClass: 'direct-link text-gray-400 dark:text-gray-600',
+			permalinkSymbol: '#',
+			permalinkAttrs: (slug, state) => ({
+				'aria-label': `permalink to ${slug}`,
+				title: 'Anchor link for easy sharing.'
+			})
+		})
+	eleventyConfig.setLibrary('md', markdownLibrary)
+
+    // Returns work items, sorted by display order
+    eleventyConfig.addCollection('work', collection => {
+      return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+    });
+
+    // Returns work items, sorted by display order then filtered by featured
+    eleventyConfig.addCollection('featuredWork', collection => {
+      return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+        x => x.data.featured
+      );
+    });
+
 
   return {
     dir: {
---
+++ src/work/behind-the-scenes.md
@@ -0,0 +1,20 @@
+---
+templateEngineOverride: njk,md
+metaTitle: 'Work Title 1'
+metaDescription: Work Title 1 Description
+title: 'Work Title 1'
+description: Work Title 1 Description
+summary: 'A travel journal that transports you to some of the most
+beautiful places on earth.'
+displayOrder: 2
+featured: true
+hero:
+  image: '/img/abhinav@0.3x.jpg'
+  imageAlt: 'A composition of all the views from the app'
+projectOverview:
+  context: 'Personal Project'
+  role: 'Product Designer'
+  deliverables: 'User Research, User Experience Design, Interaction Design, Visual Design and Prototyping'
+  timeline: '3 Weeks (April 2021)'
+  tools: 'Pen, Paper, and Figma'
+---
